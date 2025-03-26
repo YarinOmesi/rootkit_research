fd5e957b5dec254ceef4a5a64c1e96c0ee511446
YarinOmesi <yarinomesi@gmail.com>
2025-03-26 21:49:43 +0200

    save progress

--- STEP2.md
+++ STEP2.md
@@ -8,3 +8,12 @@ i think about
 - changing file entry type to `DT_UNKNOWN`
 - change file inode number (maybe cause missmatch with stat?)
 - /home/yarin/code/rootkit_research/linux/Documentation/filesystems/seq_file.rst file iterator ??
+- intercepting somehow the call to `getdents64` and filtering my entries
+  - reading about linux traps
+  - ptrace
+    - https://stackoverflow.com/questions/18577956/how-to-use-ptrace-to-get-a-consistent-view-of-multiple-threads
+    - https://security.stackexchange.com/questions/33528/force-all-user-processes-to-be-ptraced
+
+
+trying to listen to new processes and attach to them by ptrace
+then i could control syscall invocation and return values
--- rootkit/yarin_module.c
+++ rootkit/yarin_module.c
@@ -2,14 +2,43 @@
 
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/fprobe.h>
+#include <linux/ftrace.h>
+
+
+static int my_entry_handler(struct fprobe *fp, unsigned long entry_ip,
+                      unsigned long ret_ip, struct pt_regs *regs,
+                      void *entry_data){
+
+    struct ftrace_regs* ftraceRegs = (struct ftrace_regs* ) regs;
+    unsigned long syscall_name = ftrace_regs_get_argument(ftraceRegs, 0);
+    //char* value = syscall_name;
+
+    pr_info("YarinModule: entry %ld\n", syscall_name);
+    return 0;
+}
+
+static void my_exit_handler(struct fprobe *fp, unsigned long entry_ip,
+                           unsigned long ret_ip, struct pt_regs *regs,
+                           void *entry_data){
+
+    pr_info("YarinModule: exit\n");
+}
+static struct fprobe fp = {
+        .entry_handler= my_entry_handler,
+        .exit_handler= my_exit_handler
+};
+
 
 static int __init entrypoint(void){
-    pr_info("Yarin Module EntryPoint");
+    pr_info("Yarin Module EntryPoint\n");
+    register_fprobe(&fp, "syscall", "");
     return 0;
 }
 
 static void __exit cleanup(void){
-    pr_info("Yarin Module cleanup");
+    pr_info("Yarin Module cleanup\n");
+    unregister_fprobe(&fp);
 }
 
 module_init(entrypoint)
