aa73fceb60ba6f99c17e83eda2f1ea4eaf097698
YarinOmesi <yarinomesi@gmail.com>
2025-03-26 22:27:20 +0200

    save progress

--- rootkit/yarin_module.c
+++ rootkit/yarin_module.c
@@ -1,44 +1,50 @@
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
-
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/fprobe.h>
 #include <linux/ftrace.h>
+#include <linux/kprobes.h>
+
+static char symbol[KSYM_NAME_LEN] = "kernel_clone";
 
 
-static int my_entry_handler(struct fprobe *fp, unsigned long entry_ip,
-                      unsigned long ret_ip, struct pt_regs *regs,
-                      void *entry_data){
+static struct kprobe kp = {
+        .symbol_name	= symbol,
+};
 
-    struct ftrace_regs* ftraceRegs = (struct ftrace_regs* ) regs;
-    unsigned long syscall_name = ftrace_regs_get_argument(ftraceRegs, 0);
-    //char* value = syscall_name;
+static int __kprobes handler_pre(struct kprobe *p, struct pt_regs *regs)
+{
+    pr_info("<%s> p->addr = 0x%p, ip = %lx, flags = 0x%lx\n",
+            p->symbol_name, p->addr, regs->ip, regs->flags);
 
-    pr_info("YarinModule: entry %ld\n", syscall_name);
     return 0;
 }
 
-static void my_exit_handler(struct fprobe *fp, unsigned long entry_ip,
-                           unsigned long ret_ip, struct pt_regs *regs,
-                           void *entry_data){
-
-    pr_info("YarinModule: exit\n");
+static void __kprobes handler_post(struct kprobe *p, struct pt_regs *regs,
+                                   unsigned long flags)
+{
+    pr_info("<%s> p->addr = 0x%p, flags = 0x%lx\n",
+            p->symbol_name, p->addr, regs->flags);
 }
-static struct fprobe fp = {
-        .entry_handler= my_entry_handler,
-        .exit_handler= my_exit_handler
-};
-
 
 static int __init entrypoint(void){
+    int ret;
     pr_info("Yarin Module EntryPoint\n");
-    register_fprobe(&fp, "syscall", "");
+    kp.pre_handler = handler_pre;
+    kp.post_handler = handler_post;
+
+    ret = register_kprobe(&kp);
+    if (ret < 0) {
+        pr_err("register_kprobe failed, returned %d\n", ret);
+        return ret;
+    }
+    pr_info("Planted kprobe at %p\n", kp.addr);
     return 0;
 }
 
 static void __exit cleanup(void){
-    pr_info("Yarin Module cleanup\n");
-    unregister_fprobe(&fp);
+    unregister_kprobe(&kp);
+    pr_info("kprobe at %p unregistered\n", kp.addr);
 }
 
 module_init(entrypoint)
