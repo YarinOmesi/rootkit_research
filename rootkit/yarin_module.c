237836e3cef44cc0a1ce9d3ef43d8a517ad549b5
YarinOmesi <yarinomesi@gmail.com>
2025-03-29 00:54:55 +0300

    switch using kretprobe

--- rootkit/yarin_module.c
+++ rootkit/yarin_module.c
@@ -7,47 +7,65 @@
 #include <linux/kprobes.h>
 #include <linux/fprobe.h>
 
-static struct kprobe kp;
+static struct kretprobe kretp = {
+        .maxactive = 20
+};
 
-static int __kprobes handler_pre(struct kprobe *p, struct pt_regs *regs)
+struct getdent64_arguments {
+    unsigned long fd;
+    unsigned long buffer_ptr;
+    unsigned long count;
+};
+
+static int handler_pre(struct kretprobe_instance *ri, struct pt_regs *regs)
 {
+    struct getdent64_arguments* args;
     // syscall wrappers __x64_sys_* gets only ptr to regs
     regs = ((struct pt_regs*)regs->di);
 
-    unsigned long syscall = regs->orig_ax;
-    unsigned long fd = regs->di; // 0
-    unsigned long buffer_ptr = regs->si; // 1
-    unsigned long count = regs->dx; // 2
+    if (!current->mm)
+        return 1;	/* Skip kernel threads */
 
-    pr_info("getdents64(fd=%ld, buffer_ptr=%ld, count=%ld)\n",fd, buffer_ptr, count);
+
+    args = (struct getdent64_arguments*)ri->data;
+
+    //unsigned long syscall = regs->orig_ax;
+    args->fd = regs->di; // 0
+    args->buffer_ptr = regs->si; // 1
+    args->count = regs->dx; // 2
+
+    pr_info("getdents64(fd=%ld, buffer_ptr=%ld, count=%ld)\n", args->fd, args->buffer_ptr, args->count);
     return 0;
 }
 
-static void __kprobes handle_post(struct kprobe * p, struct pt_regs * regs,
-                       unsigned long flags)
+static int handle_post(struct kretprobe_instance *ri, struct pt_regs *regs)
 {
-    regs = ((struct pt_regs*)regs->di);
-    unsigned long count = regs->dx;
-    pr_info("getdents64 return = %ld\n", count);
+    struct getdent64_arguments* args = (struct getdent64_arguments*) ri->data;
+    unsigned long retval = regs_return_value(regs);
+    pr_info("getdents64 fd=%ld return = %ld\n", args->fd, retval);
+    return 0;
 }
 
 static int __init entrypoint(void)
 {
-    kp.pre_handler = handler_pre;
-    kp.post_handler = handle_post;
-    kp.symbol_name = "__x64_sys_getdents64";
+    kretp.entry_handler = handler_pre;
+    kretp.handler = handle_post;
+    kretp.kp.symbol_name = "__x64_sys_getdents64";
+    kretp.data_size = sizeof(struct getdent64_arguments);
 
-    if (register_kprobe(&kp) < 0) {
-        pr_info("register_kprobe failed\n");
+    if (register_kretprobe(&kretp) < 0) {
+        pr_info("register_kretprobe failed\n");
         return -1;
     }
-    pr_info("kprobe registered\n");
-    pr_info("Yarin Module end\n");
+    pr_info("kretprobe registered\n");
     return 0;
 }
 
 static void __exit cleanup(void){
-    unregister_kprobe(&kp);
+    pr_info("Missed probing %d instances of %s\n",
+            kretp.nmissed, kretp.kp.symbol_name);
+
+    unregister_kretprobe(&kretp);
     pr_info("Yarin Module cleanup\n");
 }
 
