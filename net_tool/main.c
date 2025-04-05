da251117a3ddaf28e7aeccfa0ff362a6d1139126
YarinOmesi <yarinomesi@gmail.com>
2025-04-06 00:00:22 +0300

    better code

--- net_tool/main.c
+++ net_tool/main.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <sys/socket.h>
-#include <sys/types.h>
 #include <string.h>
-#include <fcntl.h>
 #include <arpa/inet.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -12,66 +10,65 @@
 
 char buffer[BUFFER_SIZE];
 
-const char* SERVER = "server";
-const char* CLIENT = "client";
+const char *SERVER = "server";
+const char *CLIENT = "client";
 
-const int ARG_INDEX = 1;
+const int ARG_MODE = 1;
 const int ARG_ADDR = 2;
 const int ARG_PORT = 3;
 const int ARG_MESSAGE = 4;
 
 void print_usage();
 
-void server(in_addr_t addr,int port);
+void server(struct in_addr addr, int port);
 
-void client(in_addr_t dest_addr, int port, char* message);
+void client(struct in_addr dest_addr, int port, char *message);
 
 
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[]) {
     // zero the buffer
     memset(buffer, 0, BUFFER_SIZE);
 
     // cli <tool> <server / client> <addr> <port> <string message>?
 
-    if(argc < 2){
+    if (argc < 2) {
         printf("Not enogh arguments\n");
         print_usage();
         return 1;
     }
 
+    struct in_addr in_addr = {0};
 
-    in_addr_t in_addr;
-
+    // parse address
     {
-        char* addr = argv[ARG_ADDR];
+        char *addr = argv[ARG_ADDR];
 
-        int a, b, c, d;
+        char *ip_buffer = (char *) &in_addr.s_addr;
 
-        int count = sscanf(addr, "%d.%d.%d.%d", &a, &b, &c, &d);
-    
-        if(count != 4){
+        // big endian
+        int count = sscanf(addr, "%hhd.%hhd.%hhd.%hhd", &(ip_buffer[0]), &(ip_buffer[1]), &(ip_buffer[2]),
+                           &(ip_buffer[3]));
+
+        if (count != 4) {
             fprintf(stderr, "cant parse address %s\n", addr);
             return 1;
         }
-        // big endian
-        unsigned int addr_int = d << 24 | c << 16 | b << 8 | a;
-        in_addr = addr_int;
     }
 
     int port = atoi(argv[ARG_PORT]);
 
-    printf("%X:%d\n", in_addr, port);
+    printf("%s:%d\n", inet_ntoa(in_addr), port);
 
-    if(strcmp(argv[ARG_INDEX], SERVER) == 0){
+    if (strcmp(argv[ARG_MODE], SERVER) == 0) {
         server(in_addr, port);
-    } else if(strcmp(argv[ARG_INDEX], CLIENT) == 0){
-        if(argc == 5) {
+    } else if (strcmp(argv[ARG_MODE], CLIENT) == 0) {
+        if (argc == 5) {
             client(in_addr, port, argv[ARG_MESSAGE]);
         } else {
             print_usage();
             return 1;
         }
-        
+
     } else {
         fprintf(stderr, "Only allow server or client\n");
         print_usage();
@@ -81,65 +78,59 @@ int main(int argc, char* argv[]){
     return 0;
 }
 
-void server(in_addr_t host_addr, int port) {
-    printf("Starting server at %X:%d\n", host_addr, port);
+void server(struct in_addr host_addr, int port) {
+    printf("Starting server at %s:%d\n", inet_ntoa(host_addr), port);
     int socket_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    struct sockaddr_in addr;
-
-    memset(&addr, 0, sizeof(struct sockaddr_in));
+    struct sockaddr_in addr = {0};
 
     addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = htonl(host_addr);
+    addr.sin_addr = host_addr;
     addr.sin_port = ntohs(port);
 
 
-	int result = bind(socket_fd, (struct sockaddr*)&addr, sizeof(addr));
+    int result = bind(socket_fd, (struct sockaddr *) &addr, sizeof(addr));
 
-    if(result == -1){
+    if (result == -1) {
         fprintf(stderr, "Cant bind server %d\n", errno);
         return;
-    }else{
+    } else {
         printf("bind server\n");
     }
 
-
-    int bytes_read = recvfrom(socket_fd, buffer, BUFFER_SIZE, 0, NULL, NULL);
-    if(bytes_read == -1){
+    ssize_t bytes_read = recvfrom(socket_fd, buffer, BUFFER_SIZE, 0, NULL, NULL);
+    if (bytes_read == -1) {
         fprintf(stderr, "Error: %d\n", errno);
-    } else{
+    } else {
         printf("Message Received: %s\n", buffer);
     }
 
-    
+
     close(socket_fd);
 }
 
-void client(in_addr_t dest_addr, int port, char* message){
+void client(struct in_addr dest_addr, int port, char *message) {
     int socket_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    struct sockaddr_in addr;
-
-    memset(&addr, 0, sizeof(struct sockaddr_in));
+    struct sockaddr_in addr = {0};
 
     addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = dest_addr;
+    addr.sin_addr = dest_addr;
     addr.sin_port = ntohs(port);
 
     // Copy Message To The Buffer
     strcpy(buffer, message);
 
-    int len = strlen(message);
+    size_t len = strlen(message);
 
-    int bytes_read = sendto(socket_fd, (void*)buffer, len, 0, (struct sockaddr*)&addr, sizeof(struct sockaddr_in));
-    if(bytes_read == -1){
+    ssize_t bytes_read = sendto(socket_fd, (void *) buffer, len, 0, (struct sockaddr *) &addr,
+                                sizeof(struct sockaddr_in));
+    if (bytes_read == -1) {
         fprintf(stderr, "Error: %d\n", errno);
-    }else{
+    } else {
         printf("Message Sent: %s\n", buffer);
     }
-    
+
     close(socket_fd);
 }
-
-
 
 void print_usage() {
     printf("Usage:\n\tnetool <server/client> <port> <message>?\n\t message supported only when in client mode\n");
